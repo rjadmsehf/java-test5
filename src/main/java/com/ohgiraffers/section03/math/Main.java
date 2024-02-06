6cd21a857218dc8d59cfd5276b4b34e71ffdb278
pyunghun <jjg524@naver.com>
2024-02-06 20:41:51 +0900

    음 메소드

--- src/main/java/com/ohgiraffers/section03/math/Main.java
+++ src/main/java/com/ohgiraffers/section03/math/Main.java
@@ -1,9 +1,17 @@
 package com.ohgiraffers.section03.math;
 
+import java.util.Random;
+
 public class Main{
 
     public static void main(String[] args){
-        //
+
+        Random rendom = new Random();
+
+        int num = rendom.nextInt(10);
+        System.out.println("0 ~9까지" + num);
+
+
     }
 
 }
